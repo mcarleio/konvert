9eccc2ae5387cfe9db8d201cdb52208ca9cead13
Marcel Carlé <hi-github@mcarle.io>
2024-01-10 19:04:45 +0100

    test: add test for konvert.konverter.use-reflection

---
+++ processor/src/test/kotlin/io/mcarle/konvert/processor/KonvertOptionsITest.kt
@@ -0,0 +1,59 @@
+package io.mcarle.konvert.processor
+
+import com.tschuchort.compiletesting.SourceFile
+import io.mcarle.konvert.converter.IterableToIterableConverter
+import io.mcarle.konvert.converter.SameTypeConverter
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCompilerApi::class)
+class KonvertOptionsITest: KonverterITest() {
+
+    @Test
+    fun `konvert konverter use-reflection`() {
+        val (compilation) = compileWith(
+            enabledConverters = listOf(SameTypeConverter(), IterableToIterableConverter()),
+            code = SourceFile.kotlin(
+                name = "TestCode.kt",
+                contents =
+                """
+import io.mcarle.konvert.api.Konverter
+import io.mcarle.konvert.api.KonvertTo
+import io.mcarle.konvert.api.Konfig
+
+@Konverter
+interface PersonMapper {
+    fun toDto(person: Person): PersonDto
+}
+
+class Person(val age: String)
+class PersonDto(val age: String)
+
+@KonvertTo(GroupDto::class, options=[
+    Konfig(key="konvert.konverter.use-reflection", value="true")
+], mapFunctionName="toGroupWithReflection")
+@KonvertTo(GroupDto::class, mapFunctionName="toGroupWithoutReflection")
+class Group(val members: List<Person>)
+class GroupDto(val members: List<PersonDto>)
+                """.trimIndent()
+            )
+        )
+        val extensionFunctionCode = compilation.generatedSourceFor("GroupKonverter.kt")
+        println(extensionFunctionCode)
+
+        assertSourceEquals(
+            """
+            import io.mcarle.konvert.api.Konverter
+
+            public fun Group.toGroupWithReflection(): GroupDto = GroupDto(
+              members = members.map { Konverter.get<PersonMapper>().toDto(person = it) }
+            )
+
+            public fun Group.toGroupWithoutReflection(): GroupDto = GroupDto(
+              members = members.map { PersonMapperImpl.toDto(person = it) }
+            )
+            """.trimIndent(),
+            extensionFunctionCode
+        )
+    }
+}
