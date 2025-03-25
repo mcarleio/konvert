3ac94f6409ba813412a8c5dd2881ac745db88fb0
Marcel Carlé <hi-github@mcarle.io>
2025-03-25 23:01:35 +0100

    build: increase heap size for tests

--- buildSrc/src/main/kotlin/konvert.kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/konvert.kotlin.gradle.kts
@@ -29,3 +29,10 @@ tasks.withType<KotlinCompile>().configureEach {
     compilerOptions.jvmTarget = JvmTarget.JVM_17
     compilerOptions.javaParameters = true
 }
+
+
+tasks.test {
+    // increase memory for KSP2
+    minHeapSize = "2048m"
+    maxHeapSize = "2048m"
+}
