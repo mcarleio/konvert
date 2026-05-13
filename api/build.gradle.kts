26ebe3ab7d63017f050d4bb823bd30f3676487c7
Marcel Carlé <hi-github@mcarle.io>
2026-05-13 22:18:52 +0200

    build: adapt konvert-api build

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,45 +7,43 @@ val generatedDir = layout.buildDirectory.dir("generated/generator/kotlin")
 
 kotlin {
     jvm {
-        compilations {
-            @Suppress("UNUSED_VARIABLE")
-            val generator by compilations.creating {
-                defaultSourceSet {
-                    // Map to the existing source directory layout
-                    kotlin.srcDir("src/generator/kotlin")
+        @Suppress("UNUSED_VARIABLE")
+        val generator by compilations.creating {
+            defaultSourceSet {
+                // Map to the existing source directory layout
+                kotlin.srcDir("src/generator/kotlin")
 
-                    dependencies {
-                        rootProject.subprojects.forEach {
-                            // do not include dependency on empty injectors module, docs, annotations and api itself
-                            // as they do not contain any configurations and converters
-                            if (it.path !in arrayOf(":injectors", ":docs", ":annotations", project.path)) {
-                                implementation(project(it.path))
-                            }
+                dependencies {
+                    rootProject.subprojects.forEach {
+                        // do not include dependency on empty injectors module, docs, annotations and api itself
+                        // as they do not contain any configurations and converters
+                        if (it.path !in arrayOf(":injectors", ":docs", ":annotations", project.path)) {
+                            implementation(project(it.path))
                         }
-                        implementation("org.reflections:reflections:${Versions.orgReflections}")
-                        implementation(kotlin("reflect"))
                     }
+                    implementation("org.reflections:reflections:${Versions.orgReflections}")
+                    implementation(kotlin("reflect"))
                 }
+            }
 
-                val generateTask = tasks.register<JavaExec>("generateApiConstants") {
-                    description = "Generates constants for configurations and converters"
-                    group = LifecycleBasePlugin.BUILD_GROUP
-                    classpath = runtimeDependencyFiles + output.allOutputs
-                    mainClass.set("GenerateKt")
-                    args = listOf(generatedDir.get().asFile.absolutePath)
-                    outputs.dir(generatedDir)
-                }
+            val generateTask = tasks.register<JavaExec>("generateApiConstants") {
+                description = "Generates constants for configurations and converters"
+                group = LifecycleBasePlugin.BUILD_GROUP
+                classpath = runtimeDependencyFiles + output.allOutputs
+                mainClass.set("GenerateKt")
+                args = listOf(generatedDir.get().asFile.absolutePath)
+                outputs.dir(generatedDir)
+            }
 
-                tasks.named("jvmProcessResources") {
-                    dependsOn(generateTask)
-                }
+            tasks.named("jvmProcessResources") {
+                dependsOn(generateTask)
             }
         }
     }
 
     sourceSets {
         commonMain {
-            // Use srcDir with the task provider so that KGP automatically
+            // Use srcDir with the task provider so that Kotlin Gradle Plugin automatically
             // adds a task dependency from all compile tasks to generateApiConstants
             kotlin.setSrcDirs(emptyList<String>())
             kotlin.srcDir(tasks.named("generateApiConstants"))
