7eeab20b56ebf9cea20b5796655f0ceb8438cbd5
Marcel Carlé <hi-github@mcarle.io>
2026-05-13 22:19:00 +0200

    chore: add more target platforms

--- buildSrc/src/main/kotlin/konvert.kotlin.kmp.gradle.kts
+++ buildSrc/src/main/kotlin/konvert.kotlin.kmp.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -22,8 +23,10 @@ kover {
 kotlin {
     jvm {
         compilations.all {
-            compilerOptions.configure {
-                jvmTarget = JvmTarget.JVM_17
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget = JvmTarget.JVM_17
+                }
             }
         }
     }
@@ -31,6 +34,33 @@ kotlin {
         browser()
         nodejs()
     }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+    }
+
+    // Native targets - Tier 1
+    linuxX64()
+    macosX64()
+    macosArm64()
+
+    // Native targets - Tier 2 (iOS)
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+
+    // Native targets - Tier 2 (watchOS, tvOS)
+    watchosArm64()
+    watchosSimulatorArm64()
+    tvosArm64()
+    tvosSimulatorArm64()
+
+    // Native targets - Tier 3
+    mingwX64()
+    linuxArm64()
+
+    applyDefaultHierarchyTemplate()
 
     sourceSets {
         val jvmMain by getting {
